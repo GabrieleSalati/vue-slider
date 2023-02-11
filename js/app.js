37e582129e07e6d9bea67c9c37531ce8ca7f232a
GabrieleSalati <gabriele.salati5@gmail.com>
2023-02-11 12:53:32 +0100

    added infos and thumbnails

--- js/app.js
+++ js/app.js
@@ -6,13 +6,13 @@ createApp({
             movie: {
                 title: "Blade Runner 2049",
                 category: "Sci-fi Thriller Action",
-                length: "163 minutes",
+                length: 163,
                 images: [
-                    "img\image1.jpeg",
-                    "img\image 2.jpg",
-                    "img\image3.jpg",
-                    "img\image4.jpg",
-                    "img\image5.jpg"
+                    "img/image1.jpeg",
+                    "img/image 2.jpg",
+                    "img/image3.jpg",
+                    "img/image4.jpg",
+                    "img/image5.jpg"
                 ],
             }
         }
